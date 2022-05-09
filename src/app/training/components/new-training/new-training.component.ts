7c94f42b4cba87a0b46e6ff0e6687f8c68a89c89
Rana Hisham Foad <rana@megatrust.co>
2022-05-09 16:44:05 +0200

    ahmed

--- src/app/training/components/new-training/new-training.component.ts
+++ src/app/training/components/new-training/new-training.component.ts
@@ -2,8 +2,9 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Exercise } from '../../exercise.model';
 import { TrainingService } from '../../services/training.service';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, doc,docSnapshots,docData, collectionGroup,DocumentReference, onSnapshot, getDocs  } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-new-training',
@@ -12,6 +13,7 @@ import { Observable } from 'rxjs';
 })
 export class NewTrainingComponent implements OnInit {
   @Output() startTraining = new EventEmitter<void>();
+  doc: DocumentReference;
   newExecires$: Observable<any[]>;
   newExecires: Exercise[]; 
 
@@ -20,9 +22,22 @@ export class NewTrainingComponent implements OnInit {
     private firestore: Firestore
   ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+
+    const ref = doc(this.firestore, 'availableExercises', 'availableExercises');
+     docSnapshots(ref).pipe(map(data => console.log(data)));
+
+
     const data = collection(this.firestore, 'availableExercises');
-    this.newExecires$ = collectionData(data)
+    const ddd = getDocs(data);
+    const dd = ddd.docs.map(doc => doc.data()); 
+    console.log(getDocs(data));
+
+     
+    // collectionData(data).subscribe(res=>{console.log(res);
+   
+      
+  
   }
   onSatrtTraining(form: NgForm) {
     this.trainService.startExercise(form.value.exercise);
